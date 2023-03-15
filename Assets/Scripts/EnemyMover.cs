98bab6de360ef2a9515c38939d85a9b6ef10a937
gvarys <johnnyzaev@gmail.com>
2023-03-15 14:21:16 +0300

    Enemy now follows path

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -1,15 +1,26 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> waypoints = new List<Waypoint>();
+    [SerializeField] private float waitTime = 1f;
+    
+    private WaitForSecondsRealtime _pathWaitTime;
 
     private void Start()
     {
+        _pathWaitTime = new WaitForSecondsRealtime(waitTime);
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
         foreach (var waypoint in waypoints)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return _pathWaitTime;
         }
     }
 }
