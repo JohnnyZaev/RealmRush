b1f531c2df246eab07de78f5c2c2ede701620280
gvarys <johnnyzaev@gmail.com>
2023-03-21 18:49:57 +0300

    change camera position a little and now we can detect mouse input

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    
+    [SerializeField] private bool isPlaceable;
+    private void OnMouseDown()
+    {
+        if (isPlaceable)
+            Debug.Log(name);
+    }
 }
