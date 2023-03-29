2350828d17cceb4958216abe47a1c65c0348ab33
gvarys <johnnyzaev@gmail.com>
2023-03-29 10:24:18 +0300

    now towers find their target properly

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -3,20 +3,44 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] private Transform weapon;
+    [SerializeField] private float radius = 15f;
+    [SerializeField] private ParticleSystem projectileParticle;
     private Transform _target;
-    
-    private void Start()
-    {
-        _target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     private void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
     private void AimWeapon()
     {
+        var targetDistance = Vector3.Distance(transform.position, _target.position);
+        
         weapon.transform.LookAt(_target.transform);
+        Attack(targetDistance < radius);
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emission = projectileParticle.emission;
+        emission.enabled = isActive;
+    }
+
+    private void FindClosestTarget()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        var maxDistance = Mathf.Infinity;
+
+        foreach (var enemy in enemies)
+        {
+            var targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (!(targetDistance < maxDistance)) continue;
+            closestTarget = enemy.transform;
+            maxDistance = targetDistance;
+        }
+
+        _target = closestTarget;
     }
 }
