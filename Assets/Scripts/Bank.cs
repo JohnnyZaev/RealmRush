6365b3c3e2de6da3b6a93ddc52b68ecf2a1ef5c8
gvarys <johnnyzaev@gmail.com>
2023-03-30 10:38:14 +0300

    UI now show how much gold player have

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int startingBalance = 150;
+
+    [SerializeField] private TextMeshProUGUI displayBalance;
 
     private int _currentBalance;
     public int CurrentBalance
@@ -14,21 +17,29 @@ public class Bank : MonoBehaviour
     private void Awake()
     {
         _currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int value)
     {
         _currentBalance += Mathf.Abs(value);
+        UpdateDisplay();
     }
     
     public void Withdraw(int value)
     {
         _currentBalance -= Mathf.Abs(value);
+        UpdateDisplay();
 
         if (_currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+    private void UpdateDisplay()
+    {
+        displayBalance.text = $"Gold: {_currentBalance}";
     }
 
     private void ReloadScene()
