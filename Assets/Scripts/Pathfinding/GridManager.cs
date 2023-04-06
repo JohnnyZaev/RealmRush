89eed01b80de8d1a856fb2b6fa37e2193b2d7b5e
gvarys <johnnyzaev@gmail.com>
2023-04-06 09:02:51 +0300

    now we create virtual grid

--- Assets/Scripts/Pathfinding/GridManager.cs
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -1,15 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Pathfinding
 {
     public class GridManager : MonoBehaviour
     {
-        [SerializeField] private Node node;
+        [SerializeField] private Vector2Int gridSize;
+        
+        private Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
-        private void Start()
+        private void Awake()
         {
-            Debug.Log(node.coordinates);
-            Debug.Log(node.isWalkable);
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            for (var x = 0; x < gridSize.x; x++)
+            {
+                for (var y = 0; y < gridSize.y; y++)
+                {
+                    var coordinates = new Vector2Int(x, y);
+                    grid.Add(coordinates, new Node(coordinates, true));
+                    Debug.Log(grid[coordinates].coordinates + "=" + grid[coordinates].isWalkable);
+                }
+            }
         }
     }
 }
